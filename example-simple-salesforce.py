80be0a10eb46e8227d1a74f67da5a45abdb267d9
David Reed <david.reed@radian.biz>
2018-03-14 15:08:05 -0400

    Initial commit

---
+++ example-simple-salesforce.py
@@ -0,0 +1,27 @@
+import argparse
+import simple_salesforce
+
+a = argparse.ArgumentParser()
+
+a.add_argument('-u', '--username', dest='user')
+a.add_argument('-p', '--password', dest='password')
+a.add_argument('-t', '--security-token', dest='token')
+a.add_argument('-a', '--access-token', dest='access_token')
+a.add_argument('-i', '--instance-url', dest='instance_url')
+a.add_argument('-s', '--sandbox', dest='sandbox', action="store_true")
+
+args = a.parse_args()
+
+if None not in [args.access_token, args.instance_url]:
+    connection = simple_salesforce.Salesforce(instance_url=args.instance_url, session_id=args.access_token)
+elif None not in [args.username, args.password, args.token]:
+    connection = simple_salesforce.Salesforce(username=args.user, 
+                                              password=args.password, 
+                                              security_token=args.token, 
+                                              sandbox=args.sandbox)
+
+assert connection is not None
+
+results = connection.query('SELECT Id FROM Account')
+
+assert results is not None
