b0308169780f0363939b03b458fd6cfb2617f066
David Reed <david.reed@radian.biz>
2018-03-14 15:43:32 -0400

    Fix Python

--- example-simple-salesforce.py
+++ example-simple-salesforce.py
@@ -3,18 +3,20 @@ import simple_salesforce
 
 a = argparse.ArgumentParser()
 
-a.add_argument('-u', '--username', dest='user', default=None)
-a.add_argument('-p', '--password', dest='password', default=None)
-a.add_argument('-t', '--security-token', dest='token', default=None)
-a.add_argument('-a', '--access-token', dest='access_token', default=None)
-a.add_argument('-i', '--instance-url', dest='instance_url', default=None)
+a.add_argument('-u', '--username', dest='user')
+a.add_argument('-p', '--password', dest='password')
+a.add_argument('-t', '--security-token', dest='token')
+a.add_argument('-a', '--access-token', dest='access_token')
+a.add_argument('-i', '--instance-url', dest='instance_url')
 a.add_argument('-s', '--sandbox', dest='sandbox', action="store_true")
 
 args = a.parse_args()
 
-if None not in [args.access_token, args.instance_url]:
+connection = None
+
+if all([args.hasattr('access_token'), args.hasattr('instance_url')]):
     connection = simple_salesforce.Salesforce(instance_url=args.instance_url, session_id=args.access_token)
-elif None not in [args.username, args.password, args.token]:
+elif all([args.hasattr('username'), args.hasattr('password'), args.hasattr('token')]):
     connection = simple_salesforce.Salesforce(username=args.user, 
                                               password=args.password, 
                                               security_token=args.token, 
